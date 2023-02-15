4573ed737a7935d890b47827951192b121df658c
NduatiMichael <mknduati@gmail.com>
2023-02-15 23:32:16 +0300

    Initial main.c, main.h and AUTHORS structure

---
+++ main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * Return: no return
+ */
+void x (y)
+
+
+/**
+ * main - Entry point
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+	
+	return (0);
+}
